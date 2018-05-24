06c5a8f8827488b88d3e7531f508a6ea47260f26
mohanraj-linuxbug <mohanraj.linuxbug@gmail.com>
2018-05-24 10:58:44 +0530

    Three shell script files added

---
+++ ifstatement.sh
@@ -0,0 +1,10 @@
+
+
+A=100
+B=20
+if [$A -gt $B]
+then
+ echo"A is greater"
+else
+ echo "B is greater"
+fi
---
+++ let.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+no1=4;
+no2=5;
+
+let result=no1+no2
+echo $result
+
+let no1++
+echo $no1
+
+let no1+=5
+echo $no1
